ab13b7b9c9ec9fac84ca0395feeb5f59a7a9eaaf
Ali Cabbarov <ali.cab@div.edu.az>
2024-12-14 11:55:30 +0400

    deploy-51

--- user-ms/src/main/java/ingress/userms/controller/AuthenticationController.java
+++ user-ms/src/main/java/ingress/userms/controller/AuthenticationController.java
@@ -43,6 +43,6 @@ public class AuthenticationController {
     }
     @GetMapping("/deploy")
     public ResponseEntity<String> test() {
-        return ResponseEntity.ok("User service running successfully. Deploy Date: 2024 12 14 11:33");
+        return ResponseEntity.ok("User service running successfully. Deploy Date: 2024 12 14 11:55");
     }
 }
